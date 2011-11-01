ede7527c38ec772995539c4b4aa6903ddbbf1c26
ramsey.kant <ramsey.kant@gmail.com>
2011-11-01 02:51:33 +0000

    commit after xcode

---
+++ chatserver/chatserver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  chatserver
+//
+//  Created by Ramsey Kant on 10/31/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
