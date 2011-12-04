e9ea786361747b87fbae722f187bf4f31ec8fe19
ramsey.kant <ramsey.kant@gmail.com>
2011-12-04 20:13:49 +0000

    Fixed guiclient make target and removed temporary guibuild bash script workaround.

--- guiclient/moc_mainwindow.cpp
+++ guiclient/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sat Dec 3 22:27:01 2011
+** Created: Sun Dec 4 12:43:03 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
